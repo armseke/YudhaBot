89444da818b19375ab7febbcbb0c230cb95e9ab9
wirayudpratama <34592105+wirayudpratama@users.noreply.github.com>
2018-02-05 16:44:31 +0700

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,7 +7,7 @@ from time import sleep
 from bs4 import BeautifulSoup
 import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz
 import wikipedia, urllib
-#from gtts import gTTS
+from gtts import gTTS
 from googletrans import Translator
     
 botStart = time.time() 
